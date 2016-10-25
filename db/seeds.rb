d4e4c6171a224a0259b083b642ece5828021877f
andrewdc92 <andrew.cordivari@gmail.com>
2016-10-25 15:52:06 -0700

    finished seed data, all talks and attendees data and views renders properly

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,46 +1,9 @@
 
-[
-  {
-    "title": "CILLUM NON",
-    "abstract": "Aliqua consequat mollit Lorem dolor nulla qui sunt tempor veniam eiusmod ullamco quis commodo.",
-    "room": 873,
-    "speaker": {
-      "name": "Hendrix Carroll",
-      "company": "Songlines",
-      "email": "hendrixcarroll@songlines.com",
-      "bio": "Magna velit adipisicing ullamco sint duis nisi."
-    },
-    "attendees": [
-      {
-        "name": "Sanders Riley",
-        "company": "Comtext",
-        "email": "sandersriley@comtext.com",
-        "registered": "2015-05-24T02:15:04 +07:00"
-      },
-      {
-        "name": "Bean Cline",
-        "company": "Utarian",
-        "email": "beancline@utarian.com",
-        "registered": "2015-06-21T02:54:39 +07:00"
-      },
-      {
-        "name": "Alfreda Mitchell",
-        "company": "Dreamia",
-        "email": "alfredamitchell@dreamia.com",
-        "registered": "2015-09-22T06:35:29 +07:00"
-      }
-    ]
-  },
-  {
-    "title": "AD IPSUM",
-    "abstract": "Fugiat nisi et mollit consequat sint.",
-    "room": 343,
-    "speaker": {
-      "name": "Melody Juarez",
-      "company": "Zillatide",
-      "email": "melodyjuarez@zillatide.com",
-      "bio": "Veniam do eu quis officia enim."
-    },
-    "attendees": []
-  }
-]
+Talk.create({title: "talk 1", abstract: "biophysics", room: 1000, speaker_name: "Hendrix Carroll", company: "Songlines", email: "hc@test.com", bio: "Covered the role gravity plays on directional growth in plants."})
+Talk.create({title: "talk 2", abstract: "curvature of light and sound", room: 2000, speaker_name: "Sir Charles England", company: "Physix", email: "sce@physix.com", bio: "Covered a wide breadth of topics related to wave theory."})
+Talk.create({title: "talk 3", abstract: "dark matter and black holes", room: 3000, speaker_name: "Neil Degrasse Tyson", company: "Discovery Channel", email: "neil@discovery.com", bio: "Discussed the theories on dark matter and the various reasons why black holes are connected to time and space."})
+
+
+Attendee.create({name: "Sanders Riley", company: "Comtext", email: "sandersriley@comtext.com", registered: "2015-05-24T02:15:04 +07:00"})
+Attendee.create({name: "Bean Cline", company: "Utarian", email: "beancline@utarian.com", registered: "2015-06-21T02:54:39 +07:00"})
+Attendee.create({name: "Alfreda Mitchell", company: "Dreamia", email: "alfredamitchell@dreamia.com", registered: "2015-09-22T06:35:29 +07:00"})
